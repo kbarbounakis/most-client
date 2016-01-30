b07972b7e3b7c54f066b32caa86146624ece46e9
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2016-01-31 00:34:26 +0200

    most client initial commit

--- most-client.js
+++ most-client.js
@@ -31,9 +31,39 @@
 
 (function(undefined) {
 
+    // Create Base64 Object
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+
+    /**
+     *
+     * @constructor
+     */
     function TextUtils() {
 
     }
+
+    /**
+     * @param {string} s
+     * @returns {*}
+     */
+    TextUtils.toBase64 = function(s) {
+        if (typeof s !== 'string') {
+            return;
+        }
+        return Base64.encode(s);
+    };
+
+    /**
+     *
+     * @param {string} s
+     * @returns {*}
+     */
+    TextUtils.fromBase64 = function(s) {
+        if (typeof s !== 'string') {
+            return;
+        }
+        return Base64.decode(s);
+    };
 
     /**
      * @param {*...} f
@@ -192,7 +222,7 @@
     };
 
     /**
-     * @param {string} base - A string which represents the base URL
+     * @param {string=} base - A string which represents the MOST Web Server base URI e.g. https://www.example.com.
      * @constructor
      */
     function ClientDataContext(base) {
@@ -202,9 +232,9 @@
         this.getBase = function() {
             return (base || "/");
         };
-        var svc_ = new ClientDataServiceBase(this.getBase());
+        var svc_ = new ClientDataService(base);
         /**
-         * @returns {ClientDataServiceBase}
+         * @returns {ClientDataService}
          */
         this.getService = function() {
             return svc_;
@@ -220,10 +250,28 @@
     };
 
     /**
-     * @param {string} base - A string which represents the base URL
+     * @param {string} username
+     * @param {string} password
+     * @returns {Promise|*}
+     */
+    ClientDataContext.prototype.authenticate = function(username,password) {
+        return this.getService().execute({
+            method:"GET",
+            url:"/User/index.json",
+            data: {
+                $filter:"id eq me()"
+            },
+            headers: {
+                Authorization:"Basic " + TextUtils.toBase64(username + ":" + password)
+            }
+        });
+    };
+
+    /**
+     * @param {string} base
      * @constructor
      */
-    function ClientDataServiceBase(base) {
+    function ClientDataService(base) {
         this.getBase = function() {
             if (/\/$/.test(base)) {
                 return base;
@@ -238,7 +286,7 @@
      * @param {{method:string,url:string,data:*,headers:*}|*} options
      * @returns {Promise|*}
      */
-    ClientDataServiceBase.prototype.execute = function(options) {
+    ClientDataService.prototype.execute = function(options) {
 
         var self = this;
         return new Promise(function(resolve,reject) {
@@ -251,8 +299,8 @@
                     method:options.method || "GET",
                     url:url_,
                     data: options.data || { },
-                    dataType:"json",
-                    headers:options.headers || { }
+                    headers: options.headers || { },
+                    dataType:"json"
                 }).done(function(result) {
                     return resolve(result)
                 }).fail(function(jqXHR, textStatus, errorThrown) {
@@ -464,7 +512,7 @@
 
     /**
      * @param {string} model - The target model
-     * @param {ClientDataServiceBase|*=} service - The underlying data service
+     * @param {ClientDataService|*=} service - The underlying data service
      * @constructor
      */
     function ClientDataQueryable(model, service) {
@@ -475,15 +523,15 @@
         this.privates_ = { };
         var svc_ = service;
         /**
-         * Gets an instance of ClientDataServiceBase which represents the data service provider
+         * Gets an instance of ClientDataService which represents the data service provider
          * associated with this model
-         * @returns {ClientDataServiceBase|*}
+         * @returns {ClientDataService|*}
          */
         this.getService = function() {
             return svc_;
         };
         /**
-         * @param {ClientDataServiceBase|*} value
+         * @param {ClientDataService|*} value
          */
         this.setService = function(value) {
             svc_ = value;
@@ -1177,13 +1225,13 @@
     };
 
     /**
-     * @param {*...} s0
+     * @param {*...} s
      * @returns {ClientDataQueryable}
      */
-    ClientDataQueryable.prototype.concat = function(s0) {
+    ClientDataQueryable.prototype.concat = function(s) {
         Args.notNull(this.privates_.left,"The left operand");
         var res = 'concat(' + this.privates_.left;
-        for (var i = 0; i < arguments.length; i++) res += ',' + ClientDataQueryable.escape(s0);
+        for (var i = 0; i < arguments.length; i++) res += ',' + ClientDataQueryable.escape(arguments[i]);
         res += ')';
         this.privates_.left = res;
         return this;
@@ -1193,7 +1241,7 @@
 
     /**
      * @param {string} name
-     * @param {ClientDataServiceBase|*} service
+     * @param {ClientDataService|*} service
      * @constructor
      */
     function ClientDataModel(name, service) {
@@ -1205,8 +1253,8 @@
             return name;
         };
         /**
-         * Gets an instance of ClientDataServiceBase which represents the current data service
-         * @returns {ClientDataServiceBase|*}
+         * Gets an instance of ClientDataService which represents the current data service
+         * @returns {ClientDataService|*}
          */
         this.getService = function() {
             return service;
@@ -1304,11 +1352,12 @@
      */
     if (jQuery) {
         /**
-         * @param {string=} url
+         * @param {string=} base - A string which represents the MOST Web Server base URI e.g. https://www.example.com.
+         * If this parameter is missing the newly created data context will be associated with local server.
          * @returns {ClientDataContext}
          */
-        jQuery.context = function(url) {
-            return new ClientDataContext(url || $.context.defaults.base);
+        jQuery.context = function(base) {
+            return new ClientDataContext(base || jQuery.context.defaults.base);
         };
         jQuery.context.defaults = {
             base:"/"
